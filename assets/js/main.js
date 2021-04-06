491966db4aff0e8af851b198bd7d6ee2c36d8921
TheKipmaster <fkipman@hotmail.com>
2021-04-06 19:06:58 -0300

    Implements sidebar menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-var flkty = new Flickity( '.gallery', {
+var flkty = new Flickity('.gallery', {
   autoPlay: true,
   wrapAround: true,
   imagesLoaded: true
@@ -7,3 +7,12 @@ var flkty = new Flickity( '.gallery', {
 window.addEventListener('load', (event) => {
   flkty.resize();
 });
+
+// document.getElementsByClassName('sidebarIconToggle')[0].addEventListener('click', (event) => {
+//   if (event.target.style.position === "absolute") {
+//     event.target.style.position = "fixed";
+//     console.log(event.target);
+//   } else {
+//     event.target.style.position = "absolute";
+//   }
+// });
