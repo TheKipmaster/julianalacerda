d2045d3bdb80cf72f71052241c5dd14d30057bb3
TheKipmaster <fkipman@hotmail.com>
2021-04-10 18:11:02 -0300

    Implements testimonies page

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,12 +7,3 @@ var flkty = new Flickity('.gallery', {
 window.addEventListener('load', (event) => {
   flkty.resize();
 });
-
-// document.getElementsByClassName('sidebarIconToggle')[0].addEventListener('click', (event) => {
-//   if (event.target.style.position === "absolute") {
-//     event.target.style.position = "fixed";
-//     console.log(event.target);
-//   } else {
-//     event.target.style.position = "absolute";
-//   }
-// });
